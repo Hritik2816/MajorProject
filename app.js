1d447c3c27629fa9784fef366c7c2a9180ce3aac
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2025-04-06 15:06:16 +0530

    all set for password

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const Listing = require('./models/listing.js')
 const path = require('path')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const ExpressError = require('./utils/ExpressError.js')
-const Review = require('./models/review.js')
-const { listingSchema, reviewSchema } = require('./schema.js');
-const wrapAsync = require('./utils/wrapAsync.js');
 
 const listing = require('./routes/listing.js')
+const review = require('./routes/review.js')
 
 app.set('view engine', "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -32,62 +29,11 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+
 
 
 app.use("/listing", listing)
-
-
-// Reviews route
-//Post review route 
-
-app.post('/listing/:id/reviews', validateReview, wrapAsync(async (req, res) => {
-  let listing = await Listing.findById(req.params.id)
-  let newReview = new Review(req.body.review)
-
-  listing.reviews.push(newReview)
-
-  await newReview.save()
-  await listing.save()
-
-  // console.log("New review save");
-  // res.send("New review send")
-
-  res.redirect(`/listing/${listing._id}`)
-}))
-
-
-// delete review route 
-app.delete('/listing/:id/review/:reviewId', wrapAsync(async (req, res) => {
-  let { id, reviewId } = req.params
-
-  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-  await Review.findByIdAndDelete(reviewId)
-
-  res.redirect(`/listing/${id}`)
-
-}))
-
-// app.get('/testListing', async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "This is beautiful palace in India",
-//     description: "This is a sample listing",
-//     price: 10000,
-//     location: "Goa Beach",
-//     country: "India"
-//   })
-//   await sampleListing.save()
-//   console.log(sampleListing)
-//   res.send("Listing created")
-// })
+app.use("/listing/:id/review", review);
 
 
 //Error handle
