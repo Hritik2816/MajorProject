e0047e4223482a4e348982b756dbd8dfa8cd1060
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2025-04-16 14:09:15 +0530

    upload photo in formate of file not url

--- routes/listing.js
+++ routes/listing.js
@@ -4,10 +4,9 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const Listing = require('../models/listing.js')
 const Review = require('../models/review.js');
 const { isLoggedIn, isOwner, validateListing, validateReview } = require('../middleware.js')
-
-
 const listingController = require("../controllers/listing.js")
-
+const multer = require("multer")
+const upload = multer({ dest: "uploads/" })
 
 router.route("/")
   .get(wrapAsync(listingController.index))
